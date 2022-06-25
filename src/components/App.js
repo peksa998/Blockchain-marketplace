00e23674e39d1efd0d03cf0202dfedc8fc5134ab
Dejan Petrovic <dejanp775@gmail.com>
2022-06-26 00:00:14 +0200

    v1.5
    odradjen front end

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import logo from "../logo.png";
 import "./App.css";
 import Marketplace from "../abis/Marketplace.json";
 import Navbar from "./Navbar";
+import Main from "./Main";
 
 class App extends Component {
   async componentWillMount() {
@@ -36,7 +37,10 @@ class App extends Component {
         Marketplace.abi,
         networkData.address
       );
-      console.log(marketplace);
+      this.setState({ marketplace });
+      const productCount = await marketplace.methods.productCount().call();
+      console.log(productCount.toString());
+      this.setState({ loading: false });
     } else {
       window.alert("Marketplace contract not deployed to detected network.");
     }
@@ -50,6 +54,18 @@ class App extends Component {
       products: [],
       loading: true,
     };
+
+    this.createProduct = this.createProduct.bind(this);
+  }
+
+  createProduct(name, price) {
+    this.setState({ loading: true });
+    this.state.marketplace.methods
+      .createProduct(name, price)
+      .send({ from: this.state.account })
+      .once("receipt", (receipt) => {
+        this.setState({ loading: false });
+      });
   }
 
   render() {
@@ -58,31 +74,14 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN{" "}
-                  <u>
-                    <b>NOW! </b>
-                  </u>
-                </a>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              {this.state.loading ? (
+                <div id="loader" className="text-center">
+                  <p className="text-center">Loading...</p>
+                </div>
+              ) : (
+                <Main createProduct={this.createProduct} />
+              )}
             </main>
           </div>
         </div>
